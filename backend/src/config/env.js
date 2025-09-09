615a6da9e0fd292b01e8bd37b5d374635f5fc0c2
GobiQrs <gobinath@qrsolutions.in>
2025-09-09 20:03:00 +0530

    dum

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -11,7 +11,7 @@ if (ENV == 'LOCAL') {
     module.exports.FRONTEND_URL = 'http://localhost:8080/'
 } else if (ENV == 'TEST') {
     module.exports.DB_PORT = "27017";
-    module.exports.MONGODB_URI = "mongodb+srv://srinivasan:yG1DtYmc6q41KSi7@qrsclusterlearning.wtihbgw.mongodb.net/vehicle-platform";
+    module.exports.MONGODB_URI = "mongodb+srv://qrstestuser:BmRM7oG5i4F7@qrsdevmongo.wbo17ev.mongodb.net/vehicle-platform";
     module.exports.NODE_ENV = "development";
     module.exports.PORT = 5000;
     module.exports.JWT_SECRET = 'your-jwt-secret-key',
